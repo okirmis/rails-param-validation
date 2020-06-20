ece833397813c09a5af2a9dff19493d5f8c2dd40
Oskar Kirmis <oskar.kirmis@posteo.de>
2020-06-20 15:47:42 +0200

    Fixed infinite recursion for custom types & bumped version to 0.2.2

--- lib/rails-param-validation/validators/custom_type.rb
+++ lib/rails-param-validation/validators/custom_type.rb
@@ -3,11 +3,10 @@ module RailsParamValidation
   class CustomTypeValidator < Validator
     def initialize(type)
       super type
-      @validator = ValidatorFactory.create type.schema
     end
 
     def matches?(path, data)
-      @validator.matches? path, data
+      ValidatorFactory.create(schema.schema).matches? path, data
     end
 
     def to_openapi
