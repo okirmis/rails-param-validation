724f111fd29184ff91cf2dff45f20df181e7be28
Oskar Kirmis <okirmis@selectline.de>
2025-02-27 17:01:47 +0100

    Bugfix (v0.7.2): Rails 8 compatibility

--- lib/rails-param-validation/rails/openapi/routing_helper.rb
+++ lib/rails-param-validation/rails/openapi/routing_helper.rb
@@ -1,42 +1,36 @@
 module RailsParamValidation
-  class Formatter < ActionDispatch::Journey::Visitors::FunctionalVisitor
-    def binary(node, seed)
-      visit(node.right, visit(node.left, seed))
-    end
-
-    def nary(node, seed)
-      node.children.inject(seed) { |s, c| visit(c, s) }
-    end
-
-    def terminal(node, seed)
-      seed.map { |s| s + node.left }
-    end
-
-    def visit_GROUP(node, seed)
-      visit(node.left, seed.dup) + seed
-    end
-
-    def visit_SYMBOL(node, seed)
-      name = node.left
-      name = name[1..-1] if name[0] == ":"
-      seed.map { |s| s + "{#{name}}" }
-    end
-  end
-
   class RoutingHelper
     def self.routes_for(controller, action)
       routes = []
       Rails.application.routes.routes.each do |route|
         if route.defaults[:controller] == controller && route.defaults[:action] == action
-          Formatter.new.accept(route.path.ast, [""]).each do |path|
-            if !RailsParamValidation.openapi.skip_format_endpoints || !path.end_with?('.{format}')
-              routes.push(path: path, method: route.verb)
-            end
-          end
+          path = RoutingHelper.build_path(route.path.build_formatter, include_format: !RailsParamValidation.openapi.skip_format_endpoints)
+          routes.push(path: path, method: route.verb)
         end
       end
 
       routes
+    end
+
+    def self.build_path(formatter, include_format:)
+      parts = formatter.instance_variable_get(:@parts).map do |part|
+        case part
+        when String
+          part
+        when ActionDispatch::Journey::Format::Parameter
+          "{#{part.name}}"
+        when ActionDispatch::Journey::Format
+          if include_format
+            build_path(part, include_format: true)
+          else
+            nil
+          end
+        else
+          nil
+        end
+      end
+
+      parts.reject(&:nil?).join
     end
   end
 end
